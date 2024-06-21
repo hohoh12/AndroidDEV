79a8c316eac130a201655bf2d0cd47cb738bbff5
Isha Patel <96524214+hohoh12@users.noreply.github.com>
2024-06-21 06:08:58 +0530

    Add files via upload

---
+++ basiccomposecalculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.basiccomposecalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.example.basiccomposecalculator.ui.theme.BasicComposeCalculatorTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val calculatorViewModel = ViewModelProvider(this)[CalculatorViewModel::class.java]
+        enableEdgeToEdge()
+        setContent {
+            BasicComposeCalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CalculatorButton(modifier = Modifier.padding(innerPadding),calculatorViewModel)
+                }
+            }
+        }
+    }
+}
